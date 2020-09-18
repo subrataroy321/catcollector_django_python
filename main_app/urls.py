118ca2c09cd7a89c35ec922a62c021e266e28a5c
subrataroy321 <subrata.r321@gmail.com>
2020-09-18 19:58:13 -0400

    add cat collector

---
+++ main_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('cats/', views.cats_index, name='cats_index'),
+    path('cats/<int:cat_id>/', views.cats_show, name='cats_show'),
+]
---
+++ main_app/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Cat
+
+# Create your views (these are like your controller actions) here.
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def cats_index(request):
+    cats = Cat.objects.all()
+    return render(request, 'cats/index.html', {'cats': cats})
+
+def cats_show(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/show.html', {'cat': cat})
+
+# class Cat:
+#     def __init__(self, name, breed, description, age):
+#         self.name = name
+#         self.breed = breed
+#         self.description = description
+#         self.age = age
+
+# cats = [
+#     Cat('Lolo', 'tabby', 'foul little demon', 3),
+#     Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+#     Cat('Raven', 'black tripod', '3 legged cat', 4)
+# ]
+
