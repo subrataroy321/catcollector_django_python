9db40692bbc1c7444252fe0a79c87bdada65640c
subrataroy321 <subrata.r321@gmail.com>
2020-10-01 17:17:41 -0400

    [settings] comment out developement SECRET_KEY

--- catcollectr/settings.py
+++ catcollectr/settings.py
@@ -16,7 +16,7 @@ import os
 import socket
 import psycopg2
 import dj_database_url
-from decouple import config
+#from decouple import config
 
 DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
@@ -48,8 +48,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
+# Production
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY'] or config('SECRET_KEY')
+SECRET_KEY = os.environ['SECRET_KEY'] 
+
+# Developement
+#or config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
